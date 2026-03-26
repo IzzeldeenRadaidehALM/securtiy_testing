e54e3322f6d2a864dc0d9fa0547d909fc92377bb
IzzeldeenRadaidehALM <izzeldeen.radaideh@almosafer.com>
2026-03-26 15:28:50 +0300

    Add secret_key to secerts.json and update test.py to print it

--- test/test.py
+++ test/test.py
@@ -2,4 +2,6 @@ import json
 if __name__ == "__main__":
     with open("test/secerts.json", "r") as f:
         secrets = json.load(f)
+
     print(secrets["password"])
+    print(secrets["secret_key"])
